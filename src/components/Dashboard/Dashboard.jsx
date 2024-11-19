98d0124c783984f9018a93f5bde5e0536b5512e9
lanewells <wellsdelaneyk@gmail.com>
2024-11-19 10:11:34 -0800

    Changed url path to caspsules-list

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -17,8 +17,8 @@ const Dashboard = ({ user, handleLogout }) => {
           `${import.meta.env.VITE_BACK_END_SERVER_URL}/users/${user.id}`,
           {
             headers: {
-              Authorization: `Bearer ${token}`,
-            },
+              Authorization: `Bearer ${token}`
+            }
           }
         )
         console.log(response)
@@ -41,7 +41,7 @@ const Dashboard = ({ user, handleLogout }) => {
         <button onClick={() => navigate("/create-capsule")}>
           Create a Capsule
         </button>
-        <button onClick={() => navigate(`/view-capsules/${user.id}`)}>
+        <button onClick={() => navigate(`/capsules-list/${user.id}`)}>
           View Your Capsules
         </button>
         <button onClick={handleEditAccount}>Edit Account</button>
