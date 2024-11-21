9dc6db98e6d14b7fcdc717e69e934bce7baa12ae
alinelmuller <alinelmuller@gmail.com>
2024-11-21 14:48:05 -0700

    Fixing images and css MasterPage

--- src/MasterPage/MasterPage.jsx
+++ src/MasterPage/MasterPage.jsx
@@ -1,40 +1,57 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import "./MasterPage.css";
 
-const MasterPage = () => {
+const MasterPage = ({ user, handleLogout }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const hideHeaderFooter = location.pathname === "/signin" || location.pathname === "/signup";
+
   return (
     <>
-      <header>
-        <div className="logo">
-          <img src="../src/assets/capsule_bkg_orange.png" alt="Logo" />
-          <span>Momento In Time</span>
-        </div>
-        <div className="links-right">
-        <a href="/logout">Logout</a>
-          <a href="/notifications">Notifications</a>
-          
-        </div>
-      </header>
+      {!hideHeaderFooter && (
+        <header>
+          <div className="logo">
+            <img src="../src/assets/capsule_bkg_orange.png" alt="Logo" />
+            <span>Momento In Time</span>
+          </div>
+          <div className="links-right">
+            <button onClick={handleLogout}>Logout</button>
+            <a href="/notifications">Notifications</a>
+          </div>
+        </header>
+      )}
       <div className="master-page-content">
         <main>
           <Outlet />
         </main>
       </div>
-      <div className="bottom-menu">
-        <a href="/profile" className="menu-item">
-          <i className="fa fa-user"></i>
-          <span>Profile</span>
-        </a>
-        <a href="/capsule/create" className="menu-item">
-          <i className="fa fa-plus-circle"></i>
-          <span>Create Capsule</span>
-        </a>
-        <a href="/capsule/list" className="menu-item">
-          <i className="fa fa-list"></i>
-          <span>View Capsules</span>
-        </a>
-      </div>
+      {!hideHeaderFooter && (
+        <div className="bottom-menu">
+          <button
+            className="menu-item"
+            onClick={() => navigate(`/profile/${user.id}`)}
+          >
+            <i className="fa fa-user"></i>
+            <span>Profile</span>
+          </button>
+          <button
+            className="menu-item"
+            onClick={() => navigate(`/capsule-form/new/${user.id}`)}
+          >
+            <i className="fa fa-plus-circle"></i>
+            <span>Create Capsule</span>
+          </button>
+          <button
+            className="menu-item"
+            onClick={() => navigate(`/capsules-list/${user.id}`)}
+          >
+            <i className="fa fa-list"></i>
+            <span>View Capsules</span>
+          </button>
+        </div>
+      )}
     </>
   );
 };
