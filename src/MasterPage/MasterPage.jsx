185a3f0f3ebc378ef8c63bf8b2fa7c3adb50ecbe
alinelmuller <alinelmuller@gmail.com>
2024-11-21 13:46:25 -0700

    css MasterPage changes

--- src/MasterPage/MasterPage.jsx
+++ src/MasterPage/MasterPage.jsx
@@ -3,37 +3,39 @@ import { Outlet } from "react-router-dom";
 import "./MasterPage.css";
 
 const MasterPage = () => {
-  const toggleMenu = () => {
-    const menu = document.getElementById("myLinks");
-    menu.style.display = menu.style.display === "block" ? "none" : "block";
-  };
-
   return (
-    <div className="master-page-container">
+    <>
       <header>
-        <div className="mobile-container">
-          <div className="topnav">
-            <a href="#home" className="active">
-              Logo
-            </a>
-            <div id="myLinks">
-              <a href="#news">News</a>
-              <a href="#contact">Contact</a>
-              <a href="#about">About</a>
-            </div>
-            <a className="icon" onClick={toggleMenu} role="button" >
-              <i className="fa fa-bars"></i>
-            </a>
-          </div>
+        <div className="logo">
+          <img src="../src/assets/capsule_bkg_orange.png" alt="Logo" />
+          <span>Momento In Time</span>
+        </div>
+        <div className="links-right">
+        <a href="/logout">Logout</a>
+          <a href="/notifications">Notifications</a>
+          
         </div>
       </header>
-      <main className="master-page-content">
-        <Outlet /> {/* Nested routes will render here */}
-      </main>
-      <footer className="master-page-footer">
-        <p>&copy; 2024 Project Momento. All rights reserved.</p>
-      </footer>
-    </div>
+      <div className="master-page-content">
+        <main>
+          <Outlet />
+        </main>
+      </div>
+      <div className="bottom-menu">
+        <a href="/profile" className="menu-item">
+          <i className="fa fa-user"></i>
+          <span>Profile</span>
+        </a>
+        <a href="/capsule/create" className="menu-item">
+          <i className="fa fa-plus-circle"></i>
+          <span>Create Capsule</span>
+        </a>
+        <a href="/capsule/list" className="menu-item">
+          <i className="fa fa-list"></i>
+          <span>View Capsules</span>
+        </a>
+      </div>
+    </>
   );
 };
 
