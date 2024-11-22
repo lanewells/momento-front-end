9ee249fac614b34f8e24504f512a287817086b31
lanewells <wellsdelaneyk@gmail.com>
2024-11-21 17:59:11 -0800

    Css styling to capsules list

--- src/components/CapsulesList/CapsulesList.jsx
+++ src/components/CapsulesList/CapsulesList.jsx
@@ -1,17 +1,14 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import "./CapsulesList.css"
 
 const CapsulesList = ({ currentUser, capsules, openDetailsPage }) => {
   const [selectedType, setSelectedType] = useState("outgoing")
-
   const navigate = useNavigate()
 
   const handleCreateButton = () => {
     navigate(`/capsule-form/new/${currentUser.id}`)
   }
-
-  const handleTypeChange = (evt) => setSelectedType(evt.target.value)
 
   const capsulesOutgoingFiltered = capsules.filter(
     (capsule) => capsule.sender === currentUser.id
@@ -48,7 +45,6 @@ const CapsulesList = ({ currentUser, capsules, openDetailsPage }) => {
         onClick={() => openDetailsPage(capsule)}
       >
         <div className="container-capsule">
-          {" "}
           <img
             src="../src/assets/capsule_bkg_cream.png"
             className="image-capsule"
@@ -68,44 +64,41 @@ const CapsulesList = ({ currentUser, capsules, openDetailsPage }) => {
     selectedType === "outgoing" ? capsulesOutgoing : capsulesIncoming
 
   return (
-    <div className="item-list-container">
-      <div className="item-list-header">
-        <h2 className="item-list-heading">My Capsules</h2>
-        <button onClick={handleCreateButton} className="item-list-add-button">
-          Add New Capsule
-        </button>
+    <div className="capsules-list-container">
+      <div className="item-list-container">
+        <div className="item-list-header">
+          <h2 className="item-list-heading">My Capsules</h2>
+          <button onClick={handleCreateButton} className="item-list-add-button">
+            Add New Capsule
+          </button>
+        </div>
+
+        <div className="capsule-tabs">
+          <div
+            className={`tab ${selectedType === "outgoing" ? "active-tab" : ""}`}
+            onClick={() => setSelectedType("outgoing")}
+          >
+            Outgoing
+          </div>
+          <div
+            className={`tab ${selectedType === "incoming" ? "active-tab" : ""}`}
+            onClick={() => setSelectedType("incoming")}
+          >
+            Incoming
+          </div>
+        </div>
+
+        {!currentList.length ? (
+          <h3>
+            You don't have any {selectedType} capsules yet. Create a new capsule
+            to get started!
+          </h3>
+        ) : (
+          <>
+            <ul className="item-list">{currentList}</ul>
+          </>
+        )}
       </div>
-      <div className="radio-buttons">
-        <label htmlFor="outgoing">Outgoing</label>
-        <input
-          type="radio"
-          id="outgoing"
-          name="capsuleType"
-          value="outgoing"
-          checked={selectedType === "outgoing"}
-          onChange={handleTypeChange}
-        />
-        <label htmlFor="incoming">Incoming</label>
-        <input
-          type="radio"
-          id="incoming"
-          name="capsuleType"
-          value="incoming"
-          checked={selectedType === "incoming"}
-          onChange={handleTypeChange}
-        />
-      </div>
-      {!currentList.length ? (
-        <h3>
-          You don't have any {selectedType} capsules yet. Create a new capsule
-          to get started!
-        </h3>
-      ) : (
-        <>
-          <h2>{selectedType}</h2>
-          <ul className="item-list">{currentList}</ul>
-        </>
-      )}
     </div>
   )
 }
