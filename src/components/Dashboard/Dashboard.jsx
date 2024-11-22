d5304c2bac142b889315ea82931ded2ee862a1af
mparente88 <parente.molly@gmail.com>
2024-11-22 11:45:31 -0800

    removed links from dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,27 +1,8 @@
-import { useNavigate } from "react-router-dom"
-
-const Dashboard = ({ user, handleLogout }) => {
-  const navigate = useNavigate()
-
+const Dashboard = ({ user }) => {
   return (
     <div className="dashboard-main">
       <h1>Welcome, {user.username}!</h1>
       <img src="../assets/logo_bkg_cream.png" alt="Momento In Time" />
-      <div>
-        <button onClick={() => navigate(`/capsule-form/new/${user.id}`)}>
-          Create a Capsule
-        </button>
-        <button onClick={() => navigate(`/capsules-list/${user.id}`)}>
-          View Your Capsules
-        </button>
-        <button onClick={() => navigate("/notifications")}>
-          View Notifications
-        </button>
-        <button onClick={() => navigate(`/profile/${user.id}`)}>
-          View Profile
-        </button>
-        <button onClick={handleLogout}>Logout</button>
-      </div>
     </div>
   )
 }
