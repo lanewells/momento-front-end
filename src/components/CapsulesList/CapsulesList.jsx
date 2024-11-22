9e83b2e7d2424cf7aa2b998826ae1e9a7ec73c8b
alinelmuller <alinelmuller@gmail.com>
2024-11-21 17:46:23 -0700

    fixing my commited error

--- src/components/CapsulesList/CapsulesList.jsx
+++ src/components/CapsulesList/CapsulesList.jsx
@@ -11,16 +11,16 @@ const CapsulesList = ({ currentUser, capsules, openDetailsPage }) => {
     navigate(`/capsule-form/new/${currentUser.id}`)
   }
 
+  const handleTypeChange = (evt) => setSelectedType(evt.target.value)
+
   const capsulesOutgoingFiltered = capsules.filter(
     (capsule) => capsule.sender === currentUser.id
   )
-
   const capsulesIncomingFiltered = capsules.filter(
     (capsule) => capsule.recipient === currentUser.id
   )
-
   const capsulesOutgoing = capsulesOutgoingFiltered.map((capsule) => (
-    <li className="item-list-item" key={capsule._id}>
+    <li key={capsule._id}>
       <button
         className="clickable-area"
         onClick={() => openDetailsPage(capsule)}
@@ -42,13 +42,14 @@ const CapsulesList = ({ currentUser, capsules, openDetailsPage }) => {
   ))
 
   const capsulesIncoming = capsulesIncomingFiltered.map((capsule) => (
-    <li className="item-list-item"  key={capsule._id}>
+    <li key={capsule._id}>
       <button
         className="clickable-area"
         onClick={() => openDetailsPage(capsule)}
       >
         <div className="container-capsule">
-        <img
+          {" "}
+          <img
             src="../src/assets/capsule_bkg_cream.png"
             className="image-capsule"
             alt="Capsule icon"
@@ -74,31 +75,37 @@ const CapsulesList = ({ currentUser, capsules, openDetailsPage }) => {
           Add New Capsule
         </button>
       </div>
-      <div className="tabs">
-        <button
-          className={`tab ${selectedType === "outgoing" ? "active" : ""}`}
-          onClick={() => setSelectedType("outgoing")}
-        >
-          Outgoing
-        </button>
-        <button
-          className={`tab ${selectedType === "incoming" ? "active" : ""}`}
-          onClick={() => setSelectedType("incoming")}
-        >
-          Incoming
-        </button>
+      <div className="radio-buttons">
+        <label htmlFor="outgoing">Outgoing</label>
+        <input
+          type="radio"
+          id="outgoing"
+          name="capsuleType"
+          value="outgoing"
+          checked={selectedType === "outgoing"}
+          onChange={handleTypeChange}
+        />
+        <label htmlFor="incoming">Incoming</label>
+        <input
+          type="radio"
+          id="incoming"
+          name="capsuleType"
+          value="incoming"
+          checked={selectedType === "incoming"}
+          onChange={handleTypeChange}
+        />
       </div>
-      <div className="tab-content">
-        {!currentList.length ? (
-          <h3>
-            You don't have any {selectedType} capsules yet. Create a new capsule to get started!
-          </h3>
-        ) : (
-          <>
-            <ul className="item-list">{currentList}</ul>
-          </>
-        )}
-      </div>
+      {!currentList.length ? (
+        <h3>
+          You don't have any {selectedType} capsules yet. Create a new capsule
+          to get started!
+        </h3>
+      ) : (
+        <>
+          <h2>{selectedType}</h2>
+          <ul className="item-list">{currentList}</ul>
+        </>
+      )}
     </div>
   )
 }
